87369e7c6362de2818cd9dd01f7d8a969511b712
colobrod72 <ftx1192@gmail.com>
2026-08-28 15:14:15 +0000

    Add files via upload

---
+++ sway.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt update -y
+sudo apt updrade -y
+sudo apt install -y 
+terminology greetd wlgreet sway sway-backgrounds xwayland xorg-xwayland wlr-randr swaylock swaybg wallpaper lxqt-policykit xdg-desktop-portal xdg-desktop-portal-wlr grim slurp wl-clipboard dunst brightnessctl power-profiles-daemon playerctl swayidle kanshi rofi picom
+
+
